c50b67a29b4e039c2ea405c82a7a39c472c821aa
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2022-01-07 20:21:39 +0100

    Inizio creazione Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-      <p>Footer</p>
+  <div class="footer">
+      <ul class="contatti">
+          <li>Contatti</li>
+          <li><a href="mailto:castellazzivalentina0@gmail.com"><i class="far fa-envelope"> email</i></a></li>
+          <li>Indirizzo ufficio</li>
+          <li>Numero di telefono</li>
+      </ul>
+      <ul class="inline_menu">
+        <li>
+          <a href="https://www.instagram.com/cv_weddingsandevents/" target="_blank"><i class="fab fa-instagram"></i></a>
+        </li>
+        <li>
+          <a href="https://www.tiktok.com/@cv_weddingsandevents" target="_blank"><i class="fab fa-tiktok"></i></a>
+        </li>
+        <li>
+          <a href="" target="_blank"><i class="fab fa-twitter"></i></a>
+        </li>
+        <li>
+          <a href="" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+        </li>
+      </ul>
+      <p class="copy">&copy; Copyright protected</p>
   </div>
 </template>
 
@@ -13,6 +33,44 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.footer {
+    position: relative;
+    height: 40vh;
+    background-color: gold;
+}
+.contatti {
+    padding-top: 20px;
+    list-style: none;
+    margin-left: 20px;
+    color: white;
+    li:first-child {
+        font-size: 20px;
+        font-weight: 800;
+    }
+}
+.inline_menu {
+    list-style: none;
+    margin-left: 20px;
+    margin-top: 20px;
+    li {
+        display: inline-block;
+        margin: 5px;
+        a {
+            color: white;
+            &:hover {
+                color: #feb5af;
+            }
+        }
+    }
+}
 
+.copy {
+    font-size: 8px;
+    color: white;
+    position: absolute;
+    bottom: 5px;
+    left: 50%;
+    transform: translateX(-50%);
+}
 </style>
